3b15d99e8cc953b298aa1660017829e26c4f38ad
Carlos Piñan <carlos.pinan@gmail.com>
2020-06-17 18:06:31 -0500

    Added java tempalte

---
+++ JavaTemplate.java
@@ -0,0 +1,50 @@
+import java.io.*;
+import java.util.StringTokenizer;
+
+public class JavaTemplate {
+
+    static class InputReader {
+        public BufferedReader reader;
+        public StringTokenizer tokenizer;
+
+        public InputReader(InputStream stream) {
+            reader = new BufferedReader(new InputStreamReader(stream), 32768);
+            tokenizer = null;
+        }
+
+        public String next() {
+            while (tokenizer == null || !tokenizer.hasMoreTokens()) {
+                try {
+                    tokenizer = new StringTokenizer(reader.readLine());
+                } catch (IOException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+            return tokenizer.nextToken();
+        }
+
+        public int nextInt() {
+            return Integer.parseInt(next());
+        }
+
+    }
+
+    private void start() {
+        InputStream inputStream = System.in;
+        InputReader in = new InputReader(inputStream);
+        PrintStream out = System.out;
+
+        int T = in.nextInt();
+        for (int t = 1; t <= T; t++) {
+            out.print("Case #" + t);
+            out.println();
+        }
+        out.close();
+    }
+
+    public static void main(String[] args) {
+        JavaTemplate p = new JavaTemplate();
+        p.start();
+    }
+
+}
